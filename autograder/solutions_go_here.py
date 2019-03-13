fb80e18bedd818a8e52939e3da27615da7d0ff77
mylin <mylin@mit.edu>
2019-03-13 10:31:19 -0400

    add normalization constant for Gaussian distribution, add code for submitting numeric results

---
+++ autograder/solutions_go_here.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+This is an optional submission for the lab5 written portion autograder. 
+This is so you can check to make sure that your understanding is 
+correct before you get a headstart on the coding. You will not get 
+credit for submitting to this autograder. It's just for your testing. 
+This pset will be manually graded
+"""
+
+def answer_to_1i():
+    """
+    Return your answer to 1i in a python list, [x, y, theta]
+    """
+    return [0, 0, 0]
+
+def answer_to_1ii():
+    """
+    Return your answer to 1ii in a python list, [x, y, theta]
+    """
+    return [0, 0, 0]
+
+def answer_to_2():
+    """
+    Return your answers to 2 in a python list for the values z=0,3,5,8,10
+    Each value should be a float
+    """
+    return [0, 0, 0, 0, 0]
+
