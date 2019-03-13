dfe43ae0627fe9c99b60d21f9b30a2c069265b20
tfh <tfh@mit.edu>
2019-03-13 14:25:43 -0400

    Updated readme without autograder

--- src/particle_filter.py
+++ src/particle_filter.py
@@ -8,6 +8,10 @@ class ParticleFilter:
 
     def __init__(self):
 
+        # Get parameters
+        self.particle_filter_frame = \
+                rospy.get_param("~particle_filter_frame")
+
         # Initialize the models
         self.motion_model = MotionModel()
         self.sensor_model = SensorModel()
@@ -18,6 +22,9 @@ class ParticleFilter:
         # Make sure you include some way to initialize
         # your particles, ideally with some sort
         # of interactive interface in rviz
+        #
+        # Publish a transformation frame between the map
+        # and the particle_filter_frame.
 
 if __name__ == "__main__":
     rospy.init_node("particle_filter")
