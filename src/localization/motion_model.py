a68f7cb7ab2db0b5b3a00e74f210026670e09f1e
lcarethe <lcarethe@mit.edu>
2023-03-22 20:52:35 +0000

    added motion model code

--- src/localization/motion_model.py
+++ src/localization/motion_model.py
@@ -1,3 +1,6 @@
+import numpy as np
+import math
+
 class MotionModel:
 
     def __init__(self):
@@ -6,10 +9,8 @@ class MotionModel:
         # TODO
         # Do any precomputation for the motion
         # model here.
-
-        pass
-
         ####################################
+        pass
 
     def evaluate(self, particles, odometry):
         """
@@ -33,6 +34,28 @@ class MotionModel:
         ####################################
         # TODO
 
-        raise NotImplementedError
+        # Apply noise
+        noise_x = np.random.normal(loc=0.0,scale=1.0)
+        noise_y = np.random.normal(loc=0.0,scale=1.0)
+        noise_theta = np.random.normal(loc=0.0,scale=1.0)
+        odometry[0] += noise_x
+        odometry[1] += noise_y
+        odometry[2] += noise_theta
 
+        # Get new poses
+        dxy, dtheta = odometry[0:2], odometry[2]
+        new_particles = np.zeros_like(particles)
+        for i,particle in enumerate(particles):
+            old_theta_body = particle[2]
+            old_xy = particle.reshape(2,1)
+
+            rotation_matrix = np.array([math.cos(old_theta_body), -math.sin(old_theta_body), \
+                math.sin(old_theta_body), math.cos(old_theta_body)]).reshape(2,2)
+
+            # Put odometry into world frame
+            world_dxy = np.matmul(rotation_matrix,dxy)
+            new_world_xy = world_dxy+old_xy
+            new_particles[i,:] = np.array([new_world_xy[0], new_world_xy[1], old_theta_body+dtheta]) # add new pose
+
+        return new_particles
         ####################################
