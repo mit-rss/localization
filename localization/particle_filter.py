606c2c4972fcdf145fb0f76ec9b21e525014be56
Yohan Guyomard <yohang@mit.edu>
2024-04-01 19:57:39 +0000

    Add skeleton code according to instructions

--- localization/particle_filter.py
+++ localization/particle_filter.py
@@ -3,6 +3,7 @@ from localization.motion_model import MotionModel
 
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from sensor_msgs.msg import LaserScan
 
 from rclpy.node import Node
 import rclpy
@@ -32,23 +33,15 @@ class ParticleFilter(Node):
         scan_topic = self.get_parameter("scan_topic").get_parameter_value().string_value
         odom_topic = self.get_parameter("odom_topic").get_parameter_value().string_value
 
-        self.laser_sub = self.create_subscription(LaserScan, scan_topic,
-                                                  self.laser_callback,
-                                                  1)
-
-        self.odom_sub = self.create_subscription(Odometry, odom_topic,
-                                                 self.odom_callback,
-                                                 1)
+        self.laser_sub = self.create_subscription(LaserScan, scan_topic, self.laser_callback, 1)
+        self.odom_sub = self.create_subscription(Odometry, odom_topic, self.odom_callback, 1)
 
         #  *Important Note #2:* You must respond to pose
         #     initialization requests sent to the /initialpose
         #     topic. You can test that this works properly using the
         #     "Pose Estimate" feature in RViz, which publishes to
         #     /initialpose.
-
-        self.pose_sub = self.create_subscription(PoseWithCovarianceStamped, "/initialpose",
-                                                 self.pose_callback,
-                                                 1)
+        self.pose_sub = self.create_subscription(PoseWithCovarianceStamped, "/initialpose", self.pose_callback, 1)
 
         #  *Important Note #3:* You must publish your pose estimate to
         #     the following topic. In particular, you must use the
@@ -56,7 +49,6 @@ class ParticleFilter(Node):
         #     provide the twist part of the Odometry message. The
         #     odometry you publish here should be with respect to the
         #     "/map" frame.
-
         self.odom_pub = self.create_publisher(Odometry, "/pf/pose/odom", 1)
 
         # Initialize the models
@@ -74,10 +66,44 @@ class ParticleFilter(Node):
         #
         # Publish a transformation frame between the map
         # and the particle_filter_frame.
+    
+    def laser_callback(self, scan: LaserScan):
+        """
+        From the instructions:
+        Whenever you get sensor data use the sensor model to compute the particle probabilities.
+        Then resample the particles based on these probabilities.
+
+        Anytime the particles are update (either via the motion or sensor model), determine the
+        "average" (term used loosely) particle pose and publish that transform.
+        """
+        pass
+
+    def odom_callback(self, odom: Odometry):
+        """
+        From the instructions:
+        Whenever you get odometry data use the motion model to update the particle positions.
+
+        Anytime the particles are update (either via the motion or sensor model), determine the
+        "average" (term used loosely) particle pose and publish that transform.
+        """
+        pass
+
+    def pose_callback(self, pose: PoseWithCovarianceStamped):
+        """
+        From the instruction:
+        You will also consider how you want to initialize your particles. We recommend that you
+        should be able to use some of the interactive topics in rviz to set an initialize "guess"
+        of the robot's location with a random spread of particles around a clicked point or pose.
+        Localization without this type of initialization (aka the global localization or the
+        kidnapped robot problem) is very hard.
+        """
+        pass
 
 
 def main(args=None):
     rclpy.init(args=args)
-    pf = ParticleFilter()
-    rclpy.spin(pf)
+    try:
+        rclpy.spin(ParticleFilter())
+    except KeyboardInterrupt:
+        pass
     rclpy.shutdown()
