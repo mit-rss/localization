f8d3f67a4d899bc7882808d684a180a10fab45db
Toya Takahashi <44595617+toyat522@users.noreply.github.com>
2025-04-02 17:24:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
          ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml', 'localization/params.yaml', 'localization/test/test_params.yaml']),
+        ('share/' + package_name, ['package.xml', 'localization/params.yaml', 'localization/test/test_params.yaml', 'localization/real_params.yaml']),
         ('share/localization/launch',
          glob.glob(os.path.join('launch', '*launch.*')) + glob.glob(os.path.join('launch/unit_tests', '*launch.*'))),
         ('share/localization/test_map', glob.glob(os.path.join('test_map', '*'))),
